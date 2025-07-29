9bea55affdf9739650d5df3829781c354ed119c3
Tiago Avila <tiago.avila09@gmail.com>
2025-07-29 10:04:57 -0300

    forgot to push main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use macroquad::prelude::*;
 
-use crate::{core::board::Board, enums::{CellType, PieceType}, ui::render_engine};
+use crate::{core::{board::Board, constants::{BOARD_WIDTH, BOARD_X, BOARD_Y}, game::Game}, enums::{CellType, PieceType, RotationDirection}, ui::render_engine};
 
 mod core {
     pub mod piece;
@@ -18,36 +18,59 @@ mod enums;
 
 #[macroquad::main("Tetris Grid")]
 async fn main() {
+    let mut game = Game::new();
     let mut last_update = get_time();
-    let update_interval = 1.2; // seconds
-    let mut row: isize = 3;
-    let mut board = Board::new();
-    board.set_cell(19, 5, CellType::Filled(BLUE));
 
-
-    let mut piece = core::piece::Piece::new(&PieceType::S, core::point_2d::Point2D::new(row, 4));
+    let piece = core::piece::Piece::new(&PieceType::S, core::point_2d::Point2D::new(3, 4));
+    game.current_piece = Some(piece);
 
     loop {
         clear_background(BLACK);
-
+        
         // Handle user input
-        if is_key_released(KeyCode::Left) {
-            piece.move_left();
+        if is_key_released(KeyCode::Left) || is_key_released(KeyCode::A) {
+            game.move_piece_left();
         }
 
-        if is_key_released(KeyCode::Right) {
-            piece.move_right();
+        if is_key_released(KeyCode::Right) || is_key_released(KeyCode::D) {
+            game.move_piece_right();
+        }
+
+        if is_key_released(KeyCode::Up) || is_key_released(KeyCode::K) || is_key_released(KeyCode::W) {
+            game.rotate_piece(RotationDirection::Clockwise);
+        }
+
+        if is_key_released(KeyCode::J) || is_key_released(KeyCode::Q) {
+            game.rotate_piece(RotationDirection::CounterClockwise);
+        }
+        
+        if is_key_down(KeyCode::S) || is_key_down(KeyCode::Down) {
+            macroquad::text::draw_text("pressing s or down", BOARD_X + BOARD_WIDTH + 10.0, BOARD_Y + 20.0, 20.0, WHITE);
+            game.start_soft_drop();
+        } 
+
+        if is_key_released(KeyCode::S) || is_key_released(KeyCode::Down) {
+            game.stop_soft_drop();
+        }
+        
+        if is_key_released(KeyCode::Space) {
+            game.hard_drop();
         }
 
         // Update piece position every interval
         let now = get_time();
-        if now - last_update > update_interval {
-            row = (row + 1) % 20;
+        if now - last_update > game.fall_speed_seconds_per_line {
+            game.move_piece_down();
+            game.detect_collision();
             last_update = now;
         }
 
-        render_engine::draw_board(&board);
-        render_engine::draw_piece(&piece);
+        render_engine::draw_board(&game.board);
+        if let Some(ref mut game_piece) = game.current_piece {
+            render_engine::draw_piece(&game_piece);
+        }
+
+        macroquad::text::draw_text(&format!("Fall speed is {:.2} seconds per line", game.fall_speed_seconds_per_line), BOARD_X + BOARD_WIDTH + 10.0, BOARD_Y, 24.0, WHITE);
 
         next_frame().await;
     }
